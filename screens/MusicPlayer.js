a585105b04e3e6cb851f3465e8d4998dc555450d
sidharthmohanty <sidharthmohanty21@gmail.com>
2022-03-07 21:46:00 +0530

    added app and musicplayer component

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,19 +1,21 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
-import MusicPlayer from './screens/MusicPlayer';
 
-const App = () => {
+const MusicPlayer = () => {
   return (
     <View style={style.container}>
-      <MusicPlayer />
+      <Text>MusicPlayer</Text>
     </View>
   );
 };
 
-export default App;
+export default MusicPlayer;
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#555',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
