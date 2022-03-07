61e66d9bb4581bad65f5d3c17736ac78a6c0955d
sidharthmohanty <sidharthmohanty21@gmail.com>
2022-03-07 22:23:45 +0530

    added vector icons and slider

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,9 +1,11 @@
 import {View, Text, StyleSheet} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import React from 'react';
 
 const MusicPlayer = () => {
   return (
     <View style={style.container}>
+      <Ionicons name="heart-outline" size={30} />
       <Text>MusicPlayer</Text>
     </View>
   );
