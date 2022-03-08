91fec83800a863cbc7a435223131cab551cbae0f
sidharthmohanty <sidharthmohanty21@gmail.com>
2022-03-08 22:13:18 +0530

    added icons

--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,13 +1,43 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import React from 'react';
 
+const {width, height} = Dimensions.get('window');
 const MusicPlayer = () => {
   return (
-    <View style={style.container}>
-      <Ionicons name="heart-outline" size={30} />
-      <Text>MusicPlayer</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      <View style={style.maincontainer}>
+        <View style={style.imageWrapper}>
+          <Image />
+        </View>
+        {/* slider */}
+        {/* music controls */}
+      </View>
+      <View style={style.bottomContainer}>
+        <View style={style.bottomContainerWrapper}>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="repeat" size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="share-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="ellipsis-horizontal" size={30} color="#888888" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -17,7 +47,22 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#555',
+  },
+  maincontainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottomContainer: {
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderTopColor: '#393E46',
+    borderWidth: 1,
+  },
+  bottomContainerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
   },
 });
